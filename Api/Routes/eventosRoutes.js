2eabdd26c0a577ae0d95f77a63bdc6c15b7052a8
Alycson Denis Pereira Moreira <20221148060013@ifrn.local>
2024-04-24 18:21:41 -0300

    1. modificado o arquivo util.js( retornado o Json da Promise no resolve, esta dentro da const jsonResult). 2. inserido o return nas linhas em que temos o util dentro as funcoes pois sem o return nao seria possivel recebe-la. 3. adicionado o async, await para receber os dados da promise.

--- Api/Routes/eventosRoutes.js
+++ Api/Routes/eventosRoutes.js
@@ -1,29 +1,34 @@
 const controller = require('../Modules/Controllers/eventosController.js');
 
 module.exports = (app) => {
-    app.get('/controllers/eventos', (req, res) => {
-        let eventos = controller.Eventos();
+
+    /*app.get('/controllers/eventos', (req, res) => {
+        controller.Eventos().then(eventos => res.send(eventos));        
+    });*/
+
+    app.get('/controllers/eventos', async (req, res) => {
+        const eventos = await controller.Eventos();
         res.send(eventos);
     });
 
-    app.get('/controllers/promoveEventos',(req, res)=>{
-        let viewEventos = controller.PromoveEvento();
+    app.get('/controllers/promoveEventos', async (req, res) => {
+        const viewEventos = await controller.PromoveEvento();
         res.send(viewEventos);
     })
 
-    app.get('/controllers/eventosdias/:a/:m/:d',(req, res)=>{
+    app.get('/controllers/eventosdias/:a/:m/:d', async (req, res) => {
         let ano = req.params.a;
         let mes = req.params.m;
         let dia = req.params.d;
-        let eventosdia = controller.EventoDia(dia,mes,ano);
+        const eventosdia = await controller.EventoDia(dia, mes, ano);
         res.send(eventosdia);
     })
-    app.get('/controllers/pesquisa/:n', (req, res)=>{
+    app.get('/controllers/pesquisa/:n', async (req, res) => {
         let nome = req.params.nome;
-        let pesquisaEvento = controller.PesquisaEvento(nome);
+        const pesquisaEvento = await controller.PesquisaEvento(nome);
         res.send(pesquisaEvento)
     })
-    app.post('/controllers/cria/:n/:d/:i/:e/:v/:di/:m/:a/:h/:mi/:id',(req, res)=>{
+    app.post('/controllers/cria/:n/:d/:i/:e/:v/:di/:m/:a/:h/:mi/:id', async (req, res) => {
         let nome = req.params.n;
         let descricao = req.params.d;
         let ingresso = req.params.i;
@@ -36,7 +41,7 @@ module.exports = (app) => {
         let minutos = req.params.mi;
         //Esse tem que vim automático
         let idUsuario = req.params.id;
-        let criaEvento = controller.CriaEvento(
+        const criaEvento = await controller.CriaEvento(
             nome,
             descricao,
             ingresso,
@@ -48,54 +53,54 @@ module.exports = (app) => {
             hora,
             minutos,
             idUsuario)
-        res.send(criaEvento);     
+        res.send(criaEvento);
     })
-    app.get('/controllers/:pesquisar',(req,res)=>{
+    app.get('/controllers/:pesquisar', async (req, res) => {
         let pesquisar = req.params.pesquisar;
-        let pesquisarEventos = controller.PesquisaEvento(pesquisar);
+        const pesquisarEventos = await controller.PesquisaEvento(pesquisar);
         res.send(pesquisarEventos);
     })
 
-    app.delete('/controllers/:evdel',(req, res)=>{
+    app.delete('/controllers/:evdel', async (req, res) => {
         let deletar = req.params.evdel;
-        let deletarEvento = controller.DeletaEvento(deletar);
+        const deletarEvento = await controller.DeletaEvento(deletar);
         res.send(deletarEvento);
     })
 
-    app.put('/controllers/editdescricao/:u/:e/:desc',(req, res)=>{
+    app.put('/controllers/editdescricao/:u/:e/:desc', async (req, res) => {
         let usuario = req.params.u;
         let evento = req.params.e;
         let descricao = req.params.desc;
-        let editar = controller.EditaDescricao(usuario,evento,descricao);
+        const editar = await controller.EditaDescricao(usuario, evento, descricao);
         res.send(editar);
     })
-    app.put('/controllers/editendereco/:u/:e/:ender', (req, res)=>{
+    app.put('/controllers/editendereco/:u/:e/:ender', async (req, res) => {
         let usuario = req.params.u;
         let evento = req.params.e;
         let endereco = req.params.ender;
-        let editar = controller.EditarEndereco(usuario,evento,endereco);
+        const editar = await controller.EditarEndereco(usuario, evento, endereco);
         res.send(editar);
     })
-    app.put('/controllers/edithora/:u/:e/:h/:m', (req, res) =>{
+    app.put('/controllers/edithora/:u/:e/:h/:m', async (req, res) => {
         let usuario = req.params.u;
         let evento = req.params.e;
         let hora = req.params.h;
         let minutos = req.params.m;
-        let editarHora = controller.EditarHora(usuario,evento,hora, minutos);
+        const editarHora = await controller.EditarHora(usuario, evento, hora, minutos);
         res.send(editarHora);
     })
-    app.put('/controllers/editingress/:u/:e/:ingress',(req, res)=>{
+    app.put('/controllers/editingress/:u/:e/:ingress', async (req, res) => {
         let usuario = req.params.u;
         let evento = req.params.e;
         let ingresso = req.params.ingress;
-        let editarIngresso = controller.EditarIngresso(usuario,evento, ingresso);
+        const editarIngresso = await controller.EditarIngresso(usuario, evento, ingresso);
         res.send(editarIngresso);
     })
-    app.put('/controllers/editvagas/:u/:e/:vagas', (req, res)=>{
+    app.put('/controllers/editvagas/:u/:e/:vagas', async (req, res) => {
         let usuario = req.params.u;
         let evento = req.params.e;
         let vagas = req.params.vagas
-        let editarVagas = controller.EditarVagas(usuario,evento,vagas);
+        const editarVagas = await controller.EditarVagas(usuario, evento, vagas);
         res.send(editarVagas);
     })
 
