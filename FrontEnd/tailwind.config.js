7884d0c4abf074cc523d2c7b5d1f6ba96b703068
janinealves04 <janinealves.sil@gmail.com>
2024-05-06 17:21:39 -0300

    Adicionando paleta de cores.

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -4,8 +4,14 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    
-    extend: {},
+
+    extend: {
+      colors: {
+        'oxford-blue': 'rgba(2, 17, 39, 1)',
+        'purpureus': 'rgba(150, 73, 203, 1)',
+        'pine-green': 'rgba(19, 111, 99, 1)'
+      },
+    },
   },
   plugins: [],
 }
