e8da211fd818e44282c98e410a85c2ca6a9ec1a9
Alycson-Moreira <alycson2@gmail.com>
2024-05-12 23:02:07 -0300

    ajustado erros de duplicidade do Front-end

--- FrontEnd/src/Components/NavBar.jsx
+++ FrontEnd/src/Components/NavBar.jsx
@@ -3,8 +3,7 @@ import { useState } from 'react'
 function NavBar() {
 
   return (
-    <nav class="bg-oxford-blue py-3 px-24 fixed w-full top-0 left-0">
-      <div class="container mx-auto flex justify-between items-center ">
+    
     <nav className="bg-gray-800 py-3 px-24 fixed w-full top-0 left-0">
       <div className="container mx-auto flex justify-between items-center ">
 
@@ -13,23 +12,18 @@ function NavBar() {
 
         
         <div className="relative flex items-center">
-          <input type="text" class="bg-gray-700 text-white py-1 px-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Buscar evento..." />
-          <img src="https://icongr.am/octicons/search.svg?size=16&color=fffafa" alt="Ícone de busca" class="absolute right-3 h-6 w-6" />
+          <input type="text" class="bg-gray-700 text-white py-1 px-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Buscar Evento..." />
+          <img src="https://icongr.am/octicons/search.svg?size=16&color=fffafa" alt="Ícone de busca" className="absolute right-3 h-6 w-6" />
         </div>
-
-        <ul class="flex space-x-6">
-          <li class="nav-buttom">
-            <button class="absolute inset-y-4 right-4 px-4 py-1 bg-roxo  hover:bg-oxford-blue text-white rounded-md">Login</button>
-          <input className="bg-gray-700 text-white py-1 px-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Buscar evento..." />
-          <img src="https://icongr.am/octicons/search.svg?size=16&color=fffafa" alt="Ícone de busca" className="absolute right-3 h-6 w-6"  />
+        
+        <div className='flex '>
+          <ul className="flex space-x-6">
+            <li className="nav-item">
+              <button className="absolute inset-y-3 right-4 px-4 py-1 bg-roxo  hover:bg-oxford-blue text-white rounded-md">Login</button>
+            </li>
+          </ul>
         </div>
-
-
-        <ul className="flex space-x-6">
-          <li className="nav-item">
-            <button className="absolute inset-y-3 right-4 px-4 py-1 bg-roxo  hover:bg-oxford-blue text-white rounded-md">Login</button>
-          </li>
-        </ul>
+        
 
       </div>
     </nav>
